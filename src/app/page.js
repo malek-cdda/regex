9c52aec7b312a4117544375602d4eeb7a121ba75
malek-cdda <malek@cdda.io>
2023-10-30 10:41:22 +0600

    update regex

--- src/app/page.js
+++ src/app/page.js
@@ -6,43 +6,89 @@ export default function Home() {
   const [regexs, setRegexs] = useState("");
   const [normalText, setNormalText] = useState("");
   const [ind, setInd] = useState(null);
+  const [len, setLen] = useState(null);
+  // ^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$
   useEffect(() => {
     const value = normalText;
-    const specialCharacters = ["@", "*", "%", "."];
-    console.log(specialCharacters.join(""));
+    const specialCharacters = ["@", "*", "%", ".", "#", " ", "^", "!"];
+    const arr = [];
     const parts = [];
     const special = [];
-    let startIndex = 0;
-
+    let arr1 = [];
     for (let i = 0; i < value.length; i++) {
       if (specialCharacters.includes(value[i])) {
-        parts.push(value.substring(startIndex, i));
+        // parts.push(value.substring(startIndex, i));
+        // console.log(value[i]);
+        special.push(value[i]);
+        // const d = parts.push(value[i]);
+        // const vd = `[a-zA-Z]` + `{${value.substring(startIndex, i).length}}`;
+        // newR = newR + vd;
+        // startIndex = i + 1;
+
+        arr.push(parts.join(""));
+        arr.push(arr1.join(""));
+        parts.length = 0;
+        arr1.length = 0;
+      } else if (Number(value[i])) {
+        arr1.push(value[i]);
+        arr.push(parts.join(""));
+        arr.push(special.join(""));
+        parts.length = 0;
+        special.length = 0;
+      } else {
         parts.push(value[i]);
-        startIndex = i + 1;
+        arr.push(special.join(""));
+        arr.push(arr1.join(""));
+        arr1.length = 0;
+        special.length = 0;
       }
     }
-
-    // Add the last part (substring after the last special character)
-    parts.push(value.substring(startIndex));
-    // const specialChar = special.join("");
-    // parts.push(specialChar);
-    const r = 4;
-    // const v = `[${parts.join("")}]` + `{${r}}`;
-    let newR = "";
-    for (let j = 0; j < parts.length; j++) {
-      console.log(parts[j]);
-      const vd = `[${parts[j]}]` + `{${parts[j].length}}`;
-      newR = newR + vd;
+    if (arr1.length > 0) {
+      arr.push(arr1.join(""));
     }
-    const vds = new RegExp(newR);
-    console.log(vds);
-    setRegexs(vds);
-    const newRegex = vds.toString();
-    //   newRegex[2] = "t";
-    const v = newR.replace(newR[ind], ind);
-    console.log(v);
-    const vds1 = new RegExp(v);
-    console.log(vds1);
+    if (special.length > 0) {
+      arr.push(special.join(""));
+    }
+    if (parts.length > 0) {
+      arr.push(parts.join(""));
+    }
+    const filter = arr.filter((item) => item !== "");
+
+    console.log(filter);
+    let k = "";
+    filter.forEach((item) => {
+      if ([...item].some((char) => specialCharacters.includes(char))) {
+        // const d = parts.push(value[i]);
+        const vd = `[@#$%^&*()]` + `{${item.length}}`;
+        k = k + vd;
+      } else if (Number(item)) {
+        const vd = `[1-9]` + `{${item.length}}`;
+        k = k + vd;
+      } else {
+        const vd = `[a-zA-Z]` + `{${item.length}}`;
+        k = k + vd;
+      }
+    });
+
+    let x = `[^${ind}]`;
+    let l = len ? `{${len}}` : `{10}`;
+    const reg = new RegExp(x + k);
+    console.log(reg);
+    // console.log(reg);
+    // console.log(special);
+    // console.log(parts.join(""));
+    // parts.push(value.substring(startIndex));
+    // console.log(parts);
+    // console.log(special.join(""));
+    // console.log(newR);
+    // let x = `[^d]`;
+    // const vds = new RegExp("^" + "t" + x + newR);
+
+    setRegexs(reg);
+    // const newRegex = vds.toString();
+    // const v = newR.replace(newR[ind], ind);
+
+    // const vds1 = new RegExp(v);
   }, [normalText, ind]);
 
   return (
@@ -53,7 +99,16 @@ export default function Home() {
           onChange={(e) => setNormalText(e.target.value)}
           className="border-2"
         />
-        <input onChange={(e) => setInd(e.target.value)} className="border-2" />
+        <input
+          onChange={(e) => setInd(e.target.value)}
+          className="border-2"
+          placeholder="enter your avoid characcters"
+        />
+        {/* <input
+          onChange={(e) => setLen(e.target.value)}
+          className="border-2"
+          placeholder="enter your length"
+        /> */}
       </div>
     </main>
   );
