34d539a83839390f59a549fbe2f403c833e3203f
malek-cdda <malek@cdda.io>
2023-11-09 14:16:00 +0600

    model-erd

--- src/components/regex/rangeCheck.js
+++ src/components/regex/rangeCheck.js
@@ -1,16 +1,21 @@
 export async function rangeCheck(min, max) {
+  const newMin = Number(min);
+  const newMax = Number(max);
   let rangeValue = "";
-  if ((!min && !max) || (min && !max)) {
-    rangeValue = `{${min ? min : 1}}`;
-    // alert("minimum");
-  } else if (min && max) {
-    if (min < max) {
-      rangeValue = `{${min},${max}}`;
-    } else if (min > max) {
-      alert("minimum value must be less than maximum value");
-      rangeValue = `{1}`;
-    }
-  }
 
+  if ((!newMin && !newMax) || (newMin && !newMax)) {
+    rangeValue = `{${newMin ? newMin : 1}}`;
+    // alert("minimum");
+  } else if (newMin && newMax) {
+    if (newMin < newMax) {
+      rangeValue = `{${newMin},${newMax}}`;
+    } else {
+      console.log("min", typeof newMin, "max", typeof newMax);
+      alert("minimum value must be less than maximum value");
+      return;
+    }
+  } else {
+    rangeValue = `{1}`;
+  }
   return rangeValue;
 }
